f84d6a547b03ef62f02d95f0974986ed0b6e70ad
snj <snj2k1@gmail.com>
2023-11-22 16:35:06 +0300

    add hook, error boundary, logger

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { ErrorBoundary } from "react-error-boundary";
 
 import App from "./components/app/app";
 import { store } from "./store/store";
@@ -10,7 +11,9 @@ import CurrencyContext from "./context/currency-context/currency-context";
 ReactDOM.render(
   <Provider store={store}>
     <CurrencyContext>
-      <App />
+      <ErrorBoundary fallback={<div>Something went wrong...</div>}>
+        <App />
+      </ErrorBoundary>
     </CurrencyContext>
   </Provider>,
   document.getElementById("root"),
