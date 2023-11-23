9e7fb523bd509330dbbe6dd099055d38f01d7769
snj <snj2k1@gmail.com>
2023-11-23 14:03:30 +0300

    remake search

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,13 +3,11 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { modalReducer } from "./slices/modal-slice";
 import { authReducer } from "./slices/auth-slice";
 import { coinlistApi } from "./rtk-query/coinlist-api";
-import { searchReducer } from "./slices/search-slice";
 import { errorLoggerMiddleware } from "./middleware/logger";
 
 export const rootReducer = combineReducers({
   modal: modalReducer,
   auth: authReducer,
-  search: searchReducer,
   [coinlistApi.reducerPath]: coinlistApi.reducer,
 });
 
