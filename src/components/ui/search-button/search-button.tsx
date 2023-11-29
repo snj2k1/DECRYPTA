8af89430ff2eb64dae03a45a2ae251cf652801d7
snj <snj2k1@gmail.com>
2023-11-29 13:32:09 +0300

    add alerts and fixed search input

--- src/components/ui/search-button/search-button.tsx
+++ src/components/ui/search-button/search-button.tsx
@@ -1,21 +1,28 @@
 import { Input, InputRef } from "antd";
-import { useState, useRef } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState, useRef, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import s from "./search-button.module.scss";
 import { useDebounce } from "../../../hooks/use-debounce";
 import { Suggests } from "../suggests/suggests";
-import { useHistory } from "../../../hooks/use-history";
 
 const { Search } = Input;
 
 const SearchButton = () => {
+  const { search: searchParams } = useLocation();
+  const query = new URLSearchParams(searchParams).get("query");
+  const [searchTerm, setSearchTerm] = useState("");
   const [suggest, setSuggest] = useState("");
   const [isSearchFocused, setIsSearchFocused] = useState(false);
   const debouncedSuggest = useDebounce(suggest, 1000);
   const navigate = useNavigate();
   const searchInputRef = useRef<InputRef | null>(null);
-  const addHistory = useHistory();
+
+  useEffect(() => {
+    setSearchTerm(query ?? searchTerm);
+    setSuggest(query?.trim() ?? suggest);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
 
   const onSearch = (value: string) => {
     const trimmedValue = value.trim();
@@ -23,12 +30,6 @@ const SearchButton = () => {
       setIsSearchFocused(false);
 
       const searchUrl = `/search?query=${encodeURIComponent(trimmedValue)}`;
-      const historyData = {
-        text: trimmedValue,
-        url: searchUrl,
-      };
-
-      addHistory(historyData);
 
       navigate(searchUrl);
 
@@ -38,13 +39,19 @@ const SearchButton = () => {
     }
   };
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSuggest(e.target.value.trim());
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <div className={s.container}>
       <Search
+        value={searchTerm}
         className={s.search}
         placeholder="Search your coin..."
         onSearch={onSearch}
-        onChange={e => setSuggest(e.target.value.trim())}
+        onChange={e => handleChange(e)}
         onFocus={() => setIsSearchFocused(true)}
         onBlur={() => setTimeout(() => setIsSearchFocused(false), 500)}
         ref={searchInputRef}
